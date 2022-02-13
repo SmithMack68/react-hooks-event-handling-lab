f61e3ab3c3b8c1ee2b7fc95f23c78355e395a646
Candice Smith MacKenzie <Mahony8@aol.com>
2022-02-13 02:03:28 -0500

    completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React from "react";
+
+
+
+function Keypad(){
+
+return (
+    <div>
+        <input 
+        type="password"
+        onChange={ e => console.log("Entering password...")}
+        />
+    </div>
+   )
+}
+
+export default Keypad
